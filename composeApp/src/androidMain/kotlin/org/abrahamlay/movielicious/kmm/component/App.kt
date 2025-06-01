ff0bfd4a7770afda87078a1c1236ef2ab00e9ecd
Abraham Lay <abraham_lay@yahoo.co.id>
2025-06-01 12:37:51 +0700

    FEAT[SETUP]: delete unused class

--- composeApp/src/androidMain/kotlin/org/abrahamlay/movielicious/kmm/component/App.kt
+++ composeApp/src/androidMain/kotlin/org/abrahamlay/movielicious/kmm/component/App.kt
@@ -1,22 +1,9 @@
 package org.abrahamlay.movielicious.kmm.component
 
-import Greeting
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import movieliciouskmm.composeapp.generated.resources.Res
-import movieliciouskmm.composeapp.generated.resources.compose_multiplatform
 import org.abrahamlay.movielicious.kmm.theme.MovieliciousTheme
 
 @Composable
